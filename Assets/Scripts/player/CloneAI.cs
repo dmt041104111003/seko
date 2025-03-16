b9db756981d551f417a59d10e23de3fa9984c6b8
dmt041104111003 <daomanhtung4102003@gmail.com @example.com>
2025-03-17 02:47:44 +0700

    instantiate (player) & pathfinding

---
+++ Assets/Scripts/player/CloneAI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b6a83a32288ec02392ad682fbe1bd94c0ba7ba6593372862e739cc72f6d3c082
+size 1796
---
+++ Assets/Scripts/player/PlayerSkill.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:97f0c0c6f6a3127bd7537cc1e8bfa1cd8190117db2edb1a8fc3e191d492d2269
+size 1561
